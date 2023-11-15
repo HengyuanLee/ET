fb272f8b5f4f9f5f0cd5d4a913bb7592544196fc
vtwo <me@example.com>
2023-11-15 22:27:18 +0800

    配置修改。

--- Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/UI/UILogin/UILoginComponentSystem.cs
@@ -3,28 +3,34 @@ using UnityEngine.UI;
 
 namespace ET.Client
 {
-	[EntitySystemOf(typeof(UILoginComponent))]
-	[FriendOf(typeof(UILoginComponent))]
-	public static partial class UILoginComponentSystem
-	{
-		[EntitySystem]
-		private static void Awake(this UILoginComponent self)
-		{
-			ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
-			self.loginBtn = rc.Get<GameObject>("LoginBtn");
-			
-			self.loginBtn.GetComponent<Button>().onClick.AddListener(()=> { self.OnLogin(); });
-			self.account = rc.Get<GameObject>("Account");
-			self.password = rc.Get<GameObject>("Password");
-		}
+    [EntitySystemOf(typeof(UILoginComponent))]
+    [FriendOf(typeof(UILoginComponent))]
+    public static partial class UILoginComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this UILoginComponent self)
+        {
+            ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
+            self.loginBtn = rc.Get<GameObject>("LoginBtn");
 
-		
-		public static void OnLogin(this UILoginComponent self)
-		{
-			LoginHelper.Login(
-				self.Root(), 
-				self.account.GetComponent<InputField>().text, 
-				self.password.GetComponent<InputField>().text).Coroutine();
-		}
-	}
+            self.loginBtn.GetComponent<Button>().onClick.AddListener(() => { self.OnLogin(); });
+            self.account = rc.Get<GameObject>("Account");
+            self.password = rc.Get<GameObject>("Password");
+        }
+
+
+        public static void OnLogin(this UILoginComponent self)
+        {
+            var all = CharacterCategory.Instance.GetAll();
+            UnityEngine.Debug.Log("cahracter数据总量：" + all.Count);
+            foreach (var item in all)
+            {
+                UnityEngine.Debug.Log($"key: {item.Key}  :  value ->{item.Value.Name}");
+            }
+            LoginHelper.Login(
+                self.Root(),
+                self.account.GetComponent<InputField>().text,
+                self.password.GetComponent<InputField>().text).Coroutine();
+        }
+    }
 }
